d631976fce8ab6cbaed425a777868e298784d19c
TeePlunder <leon.bergmann23@gmail.com>
2023-02-22 22:35:28 +0100

    finish task 1 & 2

--- python/Bools/ghost-gobble-arcade-game/arcade_game.py
+++ python/Bools/ghost-gobble-arcade-game/arcade_game.py
@@ -9,7 +9,7 @@ def eat_ghost(power_pellet_active, touching_ghost):
     :return: bool - can the ghost be eaten?
     """
 
-    pass
+    return power_pellet_active and touching_ghost
 
 
 def score(touching_power_pellet, touching_dot):
@@ -20,7 +20,7 @@ def score(touching_power_pellet, touching_dot):
     :return: bool - has the player scored or not?
     """
 
-    pass
+    return touching_power_pellet or touching_dot
 
 
 def lose(power_pellet_active, touching_ghost):
