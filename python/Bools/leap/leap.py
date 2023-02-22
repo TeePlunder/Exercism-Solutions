b68f29be1916ab0a18d9e6b650522d7a94bd3403
TeePlunder <leon.bergmann23@gmail.com>
2023-02-22 23:20:03 +0100

    optemize code

--- python/Bools/leap/leap.py
+++ python/Bools/leap/leap.py
@@ -1,11 +1,5 @@
 def leap_year(year):
-    if (year % 4 == 0):
-        if (year % 100 == 0):
-            if (year % 400 == 0):
-                return True
-            return False
-        return True
-    return False
+    return year % 4 == 0 and (year % 100 != 0 or year % 400 == 0)
 
 
 print(f"1997 leap? => {leap_year(1997)}")
