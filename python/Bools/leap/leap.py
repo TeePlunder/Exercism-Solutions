c8678eed653ebb30a90c1c2a825bcb30b9991855
TeePlunder <leon.bergmann23@gmail.com>
2023-02-22 23:09:48 +0100

    solve leap methode

--- python/Bools/leap/leap.py
+++ python/Bools/leap/leap.py
@@ -1,2 +1,21 @@
 def leap_year(year):
-    pass
+    t1 = year % 4 == 0
+    t2 = year % 100 == 0
+    t3 = year % 400 == 0
+    t4 = (year % 100 and year % 400) == 0
+    t5 = year % 4 == 0 or (year % 100 and year % 400) == 0
+
+    if (year % 4 == 0):
+        if (year % 100 == 0):
+            if (year % 400 == 0):
+                return True
+            return False
+        return True
+    return False
+
+
+print(f"1997 leap? => {leap_year(1997)}")
+print(f"1996 leap? => {leap_year(1996)}")
+print(f"1900 leap? => {leap_year(1900)}")
+print(f"2000 leap? => {leap_year(2000)}")
+print(f"2028 leap? => {leap_year(2028)}")
