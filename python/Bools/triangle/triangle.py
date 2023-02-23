0388b98dbb7582901ce643e614484dc00dc8b379
TeePlunder <leon.bergmann23@gmail.com>
2023-02-23 12:50:28 +0100

    add funcitions to validate triangle & fix test errors

--- python/Bools/triangle/triangle.py
+++ python/Bools/triangle/triangle.py
@@ -1,14 +1,31 @@
 def equilateral(sides):
     a, b, c = sides
-    return a == b == c
+
+    return isValidTriangle(sides) and a == b == c
 
 
 def isosceles(sides):
     a, b, c = sides
-
-    return equilateral(sides) or a == b or a == c or b == c
+    return isValidTriangle(sides) and (a == b or a == c or b == c)
 
 
 def scalene(sides):
     a, b, c = sides
-    return a != b != c
+    return isValidTriangle(sides) and (a != b and a != c and b != c)
+
+
+def isValidTriangle(sides):
+    return noBelowZeroNumbers(sides) and validShape(sides)
+
+
+def noBelowZeroNumbers(sides):
+    noBelowZeroFound = True
+    for s in sides:
+        if (s <= 0):
+            noBelowZeroFound = False
+    return noBelowZeroFound
+
+
+def validShape(sides):
+    a, b, c = sides
+    return (a + b >= c) and (b + c >= a) and (a + c >= b)
