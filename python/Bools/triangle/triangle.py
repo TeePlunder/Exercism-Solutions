283401c7a326309c625952f93d3a072396a958ff
TeePlunder <leon.bergmann23@gmail.com>
2023-02-23 13:00:23 +0100

    use community solutions to optamize my code

--- python/Bools/triangle/triangle.py
+++ python/Bools/triangle/triangle.py
@@ -1,31 +1,23 @@
+def valid_triangle(f):
+    def inner(sides):
+        return sum(sides) > 2 * max(sides) and f(sides)
+    return inner
+
+
+@valid_triangle
 def equilateral(sides):
     a, b, c = sides
 
-    return isValidTriangle(sides) and a == b == c
+    return a == b == c
 
 
+@valid_triangle
 def isosceles(sides):
     a, b, c = sides
-    return isValidTriangle(sides) and (a == b or a == c or b == c)
+    return (a == b or a == c or b == c)
 
 
+@valid_triangle
 def scalene(sides):
     a, b, c = sides
-    return isValidTriangle(sides) and (a != b and a != c and b != c)
-
-
-def isValidTriangle(sides):
-    return noBelowZeroNumbers(sides) and validShape(sides)
-
-
-def noBelowZeroNumbers(sides):
-    noBelowZeroFound = True
-    for s in sides:
-        if (s <= 0):
-            noBelowZeroFound = False
-    return noBelowZeroFound
-
-
-def validShape(sides):
-    a, b, c = sides
-    return (a + b >= c) and (b + c >= a) and (a + c >= b)
+    return (a != b and a != c and b != c)
