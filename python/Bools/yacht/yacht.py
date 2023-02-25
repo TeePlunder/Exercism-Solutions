17297480c7760059ff3185f78cf6f662d9d321f0
TeePlunder <leon.bergmann23@gmail.com>
2023-02-25 21:23:10 +0100

    add full house logic

--- python/Bools/yacht/yacht.py
+++ python/Bools/yacht/yacht.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import List
 
 # Score categories.
@@ -9,7 +10,7 @@ THREES = (lambda dice: check_dice_for_number(dice, 3))
 FOURS = (lambda dice: check_dice_for_number(dice, 4))
 FIVES = (lambda dice: check_dice_for_number(dice, 5))
 SIXES = (lambda dice: check_dice_for_number(dice, 6))
-FULL_HOUSE = None
+FULL_HOUSE = (lambda dice: full_house(dice))
 FOUR_OF_A_KIND = (lambda dice: four_of_kind(dice))
 LITTLE_STRAIGHT = (lambda dice: 30 if sorted(dice) == [1, 2, 3, 4, 5] else 0)
 BIG_STRAIGHT = (lambda dice: 30 if sorted(dice) == [2, 3, 4, 5, 6] else 0)
@@ -27,6 +28,12 @@ def four_of_kind(dice: List[int]):
         if (dice.count(number) >= 4):
             four_times_found_element = number
     return 4 * four_times_found_element
+
+
+def full_house(dice: List[int]):
+    if (sorted(Counter(dice).values()) == [2, 3]):
+        return sum(dice)
+    return 0
 
 
 def score(dice: List[int], category):
