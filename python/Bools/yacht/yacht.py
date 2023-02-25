d91d227e22c570a07b6ea4f8ccb3256c9b48386d
TeePlunder <leon.bergmann23@gmail.com>
2023-02-25 21:13:23 +0100

    rename function & add function "four of a kind"

--- python/Bools/yacht/yacht.py
+++ python/Bools/yacht/yacht.py
@@ -3,21 +3,30 @@ from typing import List
 # Score categories.
 # Change the values as you see fit.
 YACHT = None
-ONES = (lambda dice: checkDiceForNumber(dice, 1))
-TWOS = (lambda dice: checkDiceForNumber(dice, 2))
-THREES = (lambda dice: checkDiceForNumber(dice, 3))
-FOURS = (lambda dice: checkDiceForNumber(dice, 4))
-FIVES = (lambda dice: checkDiceForNumber(dice, 5))
-SIXES = (lambda dice: checkDiceForNumber(dice, 6))
+ONES = (lambda dice: check_dice_for_number(dice, 1))
+TWOS = (lambda dice: check_dice_for_number(dice, 2))
+THREES = (lambda dice: check_dice_for_number(dice, 3))
+FOURS = (lambda dice: check_dice_for_number(dice, 4))
+FIVES = (lambda dice: check_dice_for_number(dice, 5))
+SIXES = (lambda dice: check_dice_for_number(dice, 6))
 FULL_HOUSE = None
-FOUR_OF_A_KIND = None
+FOUR_OF_A_KIND = (lambda dice: four_of_kind(dice))
 LITTLE_STRAIGHT = None
 BIG_STRAIGHT = None
 CHOICE = sum
 
 
-def checkDiceForNumber(dice: List[int], searchedNumber: int):
+def check_dice_for_number(dice: List[int], searchedNumber: int):
     return searchedNumber * dice.count(searchedNumber)
+
+
+def four_of_kind(dice: List[int]):
+    dice.sort()
+    four_times_found_element = 0
+    for number in dice:
+        if (dice.count(number) >= 4):
+            four_times_found_element = number
+    return 4 * four_times_found_element
 
 
 def score(dice: List[int], category):
