79eea966ab6e3e41983011ba326bf202a95f7c1d
TeePlunder <leon.bergmann23@gmail.com>
2023-06-23 10:51:38 +0200

    add comments to weather programm

--- go/comments/weather-forecast/weather_forecast.go
+++ go/comments/weather-forecast/weather_forecast.go
@@ -1,8 +1,11 @@
+// Package weather provides the weather for a city.
 package weather
 
+// CurrentCondition describes the condition of the city.
 var CurrentCondition string
+// CurrentLocation is the city where the condition takes place.
 var CurrentLocation string
-
+// Forecast gets the condtion for a city.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
