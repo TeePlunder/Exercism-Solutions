5cf400d707e2896e88e46a2ce93e9d1d1c3f102f
TeePlunder <leon.bergmann23@gmail.com>
2023-10-07 18:04:39 +0200

    first impression

--- c/isogram/isogram.c
+++ c/isogram/isogram.c
@@ -1,3 +1,20 @@
 #include "isogram.h"
+#include <string.h>
+
 bool is_isogram(const char phrase[]) {
+  int lengthOfPhrase = (int)strlen(phrase);
+  int letterFoundCount = 0;
+  for (int i = 0; i < lengthOfPhrase; i++) {
+    char currentLetterToCheck = phrase[i];
+    for (int j = 0; j < lengthOfPhrase; j++) {
+      char currentLetter = phrase[j];
+      if (currentLetter == currentLetterToCheck) {
+        if (letterFoundCount >= 1) {
+          return false;
+        }
+        letterFoundCount++;
+      }
+    }
+  }
+  return true;
 }
