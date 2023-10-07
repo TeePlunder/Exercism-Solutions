b2d7b24443fd5675013b5cdcc6fa87813698dbad
TeePlunder <leon.bergmann23@gmail.com>
2023-10-07 17:46:00 +0200

    better and shorter solution

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,25 +1,12 @@
 #include "armstrong_numbers.h"
 #include <math.h>
+#include <stdio.h>
 
-int getDigitInInt(int givenInt, int position) {
-  while (--position) {
-    givenInt /= 10;
-  }
-  return givenInt % 10;
-}
-
-bool is_armstrong_number(int toCheckNumber) {
-  if (toCheckNumber < 10) {
-    return true;
-  }
-
-  int digitCount = log10(toCheckNumber) + 1;
-  int total = 0;
-
-  for (int i = 0; i < digitCount; i++) {
-    int currentDigit = getDigitInInt(toCheckNumber, i + 1);
-    total += pow(currentDigit, digitCount);
-  }
-
-  return toCheckNumber == total;
+bool is_armstrong_number(int candidate)
+{
+    int res = 0;
+    int digit_count = log10(candidate) + 1;
+    for (int i = candidate; i > 0; i /= 10)
+        res += pow(i % 10, digit_count);
+    return candidate == res;
 }
