6c7eeb3f18b54148e90f04db516d0409957d168f
TeePlunder <leon.bergmann23@gmail.com>
2023-10-07 17:30:42 +0200

    current status

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -1,1 +1,18 @@
 #include "armstrong_numbers.h"
+#include <math.h>
+
+bool is_armstrong_number(int toCheckNumber) {
+  if (toCheckNumber < 10) {
+    return true;
+  }
+int digit_count = log10(toCheckNumber) + 1;
+    int digitCount = toCheckNumber % 10;
+    int total = 0;
+    while (toCheckNumber < 0){
+
+    for (int i = 0; i < digitCount; i++) {
+    }    
+    }
+
+    return false;
+}
