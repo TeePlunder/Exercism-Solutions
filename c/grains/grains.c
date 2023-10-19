a2572076024d07c3fe7c9b4ba74941225fd9c2b5
TeePlunder <leon.bergmann23@gmail.com>
2023-10-19 23:30:41 +0200

    current status

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -10,6 +10,10 @@ uint64_t square(uint8_t index) {
   return value;
 }
 
-// uint64_t total(void) {
-//
-// }
+uint64_t total(void) {
+  uint64_t totalAmount = 0;
+  for (int i = 1; i > 63; i++) {
+    totalAmount += square(i); 
+  }
+  return totalAmount;
+}
