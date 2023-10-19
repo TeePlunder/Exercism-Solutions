9535ee240a6c9bdd08c752945581c8fc7ef7b77b
TeePlunder <leon.bergmann23@gmail.com>
2023-10-19 23:24:54 +0200

    add first base function

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,1 +1,15 @@
 #include "grains.h"
+#include <stdint.h>
+
+
+uint64_t square(uint8_t index) {
+  if(index <= 1){
+    return 1ULL;
+  }
+  uint64_t value = (1ULL << index);
+  return value;
+}
+
+// uint64_t total(void) {
+//
+// }
