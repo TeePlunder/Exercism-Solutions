62f21dd3d47f9f8f923cc9451703ec8dd77097f6
TeePlunder <leon.bergmann23@gmail.com>
2023-10-26 21:40:50 +0200

    optimize square method

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -3,20 +3,12 @@
 #include <stdio.h>
 
 uint64_t square(uint8_t index) {
-  if (index == 0 || index > 64) {
+  if (index < 1 || index > 64) {
     return 0ULL;
   }
-
-  if (index <= 1) {
-    return 1ULL;
-  }
-
-  if (index <= 2) {
-    return 2ULL;
-  }
-
-  uint64_t value = (1ULL << (index - 1));
-  return value;
+  
+  uint64_t value = 1;
+  return value << (index - 1);
 }
 
 uint64_t total(void) {
