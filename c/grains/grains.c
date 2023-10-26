f1c2f896eae837f10128c7b60ff45b7c5896280f
TeePlunder <leon.bergmann23@gmail.com>
2023-10-26 21:43:33 +0200

    format

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -6,7 +6,7 @@ uint64_t square(uint8_t index) {
   if (index < 1 || index > 64) {
     return 0ULL;
   }
-  
+
   uint64_t value = 1;
   return value << (index - 1);
 }
@@ -14,7 +14,7 @@ uint64_t square(uint8_t index) {
 uint64_t total(void) {
   uint64_t totalAmount = 0;
   uint64_t one = 1;
-  for(uint8_t i = 0; i < 64; i++){
+  for (uint8_t i = 0; i < 64; i++) {
     totalAmount += one << i;
   }
   return totalAmount;
