4ab7729cb5b5bc293ae94753b770085583353642
TeePlunder <leon.bergmann23@gmail.com>
2023-10-26 21:27:11 +0200

    fix most of the square tests

--- c/grains/grains.c
+++ c/grains/grains.c
@@ -1,19 +1,20 @@
 #include "grains.h"
 #include <stdint.h>
-
+#include <stdio.h>
 
 uint64_t square(uint8_t index) {
-  if(index <= 1){
+  if (index <= 2) {
     return 1ULL;
   }
-  uint64_t value = (1ULL << index);
+  printf("%hhu", index);
+  uint64_t value = (1ULL << (index - 1));
   return value;
 }
 
 uint64_t total(void) {
   uint64_t totalAmount = 0;
   for (int i = 1; i > 63; i++) {
-    totalAmount += square(i); 
+    totalAmount += square(i);
   }
   return totalAmount;
 }
