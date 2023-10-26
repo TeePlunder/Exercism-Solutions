b3067c7e50f1a4130afdeb20e65ca5e9c9db9184
TeePlunder <leon.bergmann23@gmail.com>
2023-10-26 21:55:08 +0200

    solve first test

--- c/difference-of-squares/difference_of_squares.c
+++ c/difference-of-squares/difference_of_squares.c
@@ -1,1 +1,11 @@
 #include "difference_of_squares.h"
+#include <math.h>
+unsigned int square_of_sum(unsigned int number){
+  unsigned int sum = 0; 
+
+  for(unsigned int i = 0; i <= number; i++) {
+    sum += i;
+  }
+
+  return pow(sum, 2);
+}
