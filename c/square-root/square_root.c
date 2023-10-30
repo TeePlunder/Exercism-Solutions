17ab7afe347d9105090cdc840565be1a5bbb2d54
TeePlunder <leon.bergmann23@gmail.com>
2023-10-30 15:07:02 +0100

    finish square root exercise

--- c/square-root/square_root.c
+++ c/square-root/square_root.c
@@ -1,1 +1,19 @@
 #include "square_root.h"
+#include <stdint.h>
+
+uint16_t square_root(uint16_t n)
+{
+  uint16_t lower = 1;
+  uint16_t upper = 256;
+  while(1) {
+    uint16_t mid = (upper + lower) / 2;
+    uint16_t square = mid * mid;
+    if(square < n)
+      lower = mid;
+    else if(square > n)
+      upper = mid;
+    else
+      return mid;
+  }
+
+}
