8c188accaca9f445b30c1c5eb5f181e02fd9d2ac
TeePlunder <leon.bergmann23@gmail.com>
2023-11-08 07:49:31 +0100

    format

--- c/armstrong-numbers/armstrong_numbers.c
+++ c/armstrong-numbers/armstrong_numbers.c
@@ -2,11 +2,10 @@
 #include <math.h>
 #include <stdio.h>
 
-bool is_armstrong_number(int candidate)
-{
-    int res = 0;
-    int digit_count = log10(candidate) + 1;
-    for (int i = candidate; i > 0; i /= 10)
-        res += pow(i % 10, digit_count);
-    return candidate == res;
+bool is_armstrong_number(int candidate) {
+  int res = 0;
+  int digit_count = log10(candidate) + 1;
+  for (int i = candidate; i > 0; i /= 10)
+    res += pow(i % 10, digit_count);
+  return candidate == res;
 }
--- c/grade-school/grade_school.c
+++ c/grade-school/grade_school.c
@@ -1,1 +1,11 @@
 #include "grade_school.h"
+typedef struct {
+   uint8_t grade;
+   char name[MAX_NAME_LENGTH];
+} student_t;
+
+typedef struct {
+   size_t count;
+   student_t students[MAX_STUDENTS];
+} roster_t;
+
